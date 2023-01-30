67a127b19049bc323d7ec572f3a91a3bd76bc539
Eren Yarar <eren2yarar1@gmail.com>
2023-01-30 16:58:59 +0300

    newObserver script

---
+++ Assets/Scripts/Observer2/MovementSubject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class MovementSubject: MonoBehaviour
+// {
+//     private List<IMovementObserver> observers = new List<IMovementObserver>();
+
+//     public void AddObserver(IMovementObserver observer)
+//     {
+//         observers.Add(observer);
+//     }
+
+//     public void RemoveObserver(IMovementObserver observer)
+//     {
+//         observers.Remove(observer);
+//     }
+
+//     public void NotifyObservers(string direction)
+//     {
+//         foreach (IMovementObserver observer in observers)
+//         {
+//             observer.OnMovement(direction);
+//         }
+//     }
+// }
---
+++ Assets/Scripts/Observer2/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class Player : IMovementObserver
+// {
+//     public void OnMovement(string direction)
+//     {
+//         switch (direction)
+//         {
+//             case "up":
+//                 transform.position += Vector3.up;
+//                 break;
+//             case "down":
+//                 transform.position += Vector3.down;
+//                 break;
+//             case "left":
+//                 transform.position += Vector3.left;
+//                 break;
+//             case "right":
+//                 transform.position += Vector3.right;
+//                 break;
+//         }
+//     }
+// }
